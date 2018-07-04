48d515276c752e3e393167c62866eb29baa650d4
Olatunbosun Adeoye <adex001@gmail.com>
2018-07-04 01:33:03 +0100

    finished implementing tests

--- server/app.js
+++ server/app.js
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000 || process.env.port;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'You reached my homepage successfully.'
+  });
+});
+
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 })
+
+export default app; // For testing purposes
